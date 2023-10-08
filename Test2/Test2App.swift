3b5819370d63cbae89be6fced72ef9da82f9ab9a
Sara <dev.SaraAlmashharawi@gmail.com>
2023-10-08 14:43:00 +0300

    Initial Commit

---
+++ Test2/Test2App.swift
@@ -0,0 +1,17 @@
+//
+//  Test2App.swift
+//  Test2
+//
+//  Created by Sara Ibrahim Almashharawi on 08/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
